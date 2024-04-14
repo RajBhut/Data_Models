d5710338b0c4f2628aac26649426de0e797bb516
Raj_Bhut <rajbhut2832005@gmail.com>
2024-04-14 09:56:54 +0530

    correcting git problem

--- Modeles/todos/sub_todos.models.js
+++ Modeles/todos/sub_todos.models.js
@@ -5,6 +5,20 @@ import mongoose from 'mongoose'
 
 const subtodos_schema = new  mongoose.Schema({
 
+    content :
+    {
+        type : String,
+        required : true 
+    },
+    complete :{
+        type : Boolean ,
+        default : false 
+    },
+    createdby :{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }
+
 },{timestamps : true})
 
 export const Sub_todos = mongoose.model("subToDo", subtodos_schema)
